9391f37596dd22aeb98a16d58f2f7de1ac5eb205
Fabian Zeiher <fzeiher@gmail.com>
2016-10-29 22:09:47 +0200

    feat: load data from the USGS Servers in Async Background Thread

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,48 +17,62 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    private static final String USGS_REQUEST_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=5&limit=10";
+
+    private EarthquakeAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a list of earthquakes.
-        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        // Download earthquake data
+        new DownloadTask().execute(USGS_REQUEST_URL);
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
+        mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         if (earthquakeListView != null) {
-            earthquakeListView.setAdapter(adapter);
+            earthquakeListView.setAdapter(mAdapter);
+        } else {
+            Toast.makeText(EarthquakeActivity.this, "Could not find any earthquakes to display", Toast.LENGTH_LONG).show();
         }
-
         //ClickListener for the Earthquake objects in the List
         if (earthquakeListView != null) {
             earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     //the clicked item is found by position and its url obtained
-                    Earthquake earthquake = earthquakes.get(position);
-                    String earthquakeUrl = earthquake.getUrl();
-                    //this method sends implicit intent to open a website
-                    openWebPage(earthquakeUrl);
+                    Earthquake earthquake = mAdapter.getItem(position);
+                    if (earthquake !=null) {
+                        String earthquakeUrl = earthquake.getUrl();
+                        //this method sends implicit intent to open a website
+                        openWebPage(earthquakeUrl);
+                    } else {
+                        Log.e(LOG_TAG, "Not able to get earthquake object from Adapter");
+                        Toast.makeText(EarthquakeActivity.this, "Sry, no URL available", Toast.LENGTH_LONG).show();
+                    }
                 }
             });
         }
@@ -70,6 +84,26 @@ public class EarthquakeActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
+        }
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, List<Earthquake>> {
+        @Override
+        protected List<Earthquake> doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            return QueryUtils.fetchEarthquakeData(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> earthquakeList) {
+            //Clear the Adapter before updating UI with new data
+            mAdapter.clear();
+
+            if (earthquakeList != null && !earthquakeList.isEmpty()) {
+                mAdapter.addAll(earthquakeList);
+            }
         }
     }
 }
--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -36,55 +36,56 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
         Earthquake currentEarthquake = getItem(position);
 
-        //get Magnitude double value and convert to String with one decimal number
-        DecimalFormat formatter = new DecimalFormat("0.0");
-        String earthquakeMagnitude = formatter.format(currentEarthquake.getMagnitude());
-        TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.text_view_magnitude);
-        magnitudeTextView.setText(earthquakeMagnitude);
+        if (currentEarthquake != null) {
+            //get Magnitude double value and convert to String with one decimal number
+            DecimalFormat formatter = new DecimalFormat("0.0");
+            String earthquakeMagnitude = formatter.format(currentEarthquake.getMagnitude());
+            TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.text_view_magnitude);
+            magnitudeTextView.setText(earthquakeMagnitude);
 
-        // Set the proper background color on the magnitude circle.
-        // Fetch the background from the TextView, which is a GradientDrawable.
-        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeTextView.getBackground();
+            // Set the proper background color on the magnitude circle.
+            // Fetch the background from the TextView, which is a GradientDrawable.
+            GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeTextView.getBackground();
 
-        // Get the appropriate background color based on the current earthquake magnitude
-        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+            // Get the appropriate background color based on the current earthquake magnitude
+            int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
 
-        // Set the color on the magnitude circle
-        magnitudeCircle.setColor(magnitudeColor);
-        //get Earthquake Place
-        //separate place into offset and primary location by index of word "by"
-        //if data does not give an offset location we use Near the + primary
+            // Set the color on the magnitude circle
+            magnitudeCircle.setColor(magnitudeColor);
 
-        String place = currentEarthquake.getPlace();
-        String offsetLocation = getContext().getString(R.string.near_the);
-        String primaryLocation = place;
-        int index = place.indexOf("of");
-        if (index !=-1) {
-            offsetLocation = place.substring(0, index + 2);
-            primaryLocation = place.substring(index + 3);
+            //get Earthquake Place
+            //separate place into offset and primary location by index of word "by"
+            //if data does not give an offset location we use Near the + primary
+            String place = currentEarthquake.getPlace();
+            String offsetLocation = getContext().getString(R.string.near_the);
+            String primaryLocation = place;
+            int index = place.indexOf("of");
+            if (index != -1) {
+                offsetLocation = place.substring(0, index + 2);
+                primaryLocation = place.substring(index + 3);
+            }
+
+            //primary Location String for first TextView
+            TextView primaryLocationTextView = (TextView) listItemView.findViewById(R.id.text_view_location_primary);
+            primaryLocationTextView.setText(primaryLocation);
+
+            //offset Location for second TextView
+            TextView offsetLocationTextView = (TextView) listItemView.findViewById(R.id.text_view_location_offset);
+            offsetLocationTextView.setText(offsetLocation);
+
+            //create date Object to be formatted from Time long
+            Date dateObject = new Date(currentEarthquake.getTime());
+
+            //Date Object is formatted by formatDate method and returned Date String sent to third TextView
+            TextView dateTextView = (TextView) listItemView.findViewById(R.id.text_view_date);
+            String formattedDate = formatDate(dateObject);
+            dateTextView.setText(formattedDate);
+
+            //Date Object is formatted by formatTime method and returned Time String sent to fourth TextView
+            TextView timeTextView = (TextView) listItemView.findViewById(R.id.text_view_time);
+            String formattedTime = formatTime(dateObject);
+            timeTextView.setText(formattedTime);
         }
-
-        //primary Location String for first TextView
-        TextView primaryLocationTextView = (TextView) listItemView.findViewById(R.id.text_view_location_primary);
-        primaryLocationTextView.setText(primaryLocation);
-
-        //offset Location for second TextView
-        TextView offsetLocationTextView = (TextView) listItemView.findViewById(R.id.text_view_location_offset);
-        offsetLocationTextView.setText(offsetLocation);
-
-        //create date Object to be formatted from Time long
-        Date dateObject = new Date(currentEarthquake.getTime());
-
-        //Date Object is formatted by formatDate method and returned Date String sent to third TextView
-        TextView dateTextView = (TextView) listItemView.findViewById(R.id.text_view_date);
-        String formattedDate = formatDate(dateObject);
-        dateTextView.setText(formattedDate);
-
-        //Date Object is formatted by formatTime method and returned Time String sent to fourth TextView
-        TextView timeTextView = (TextView) listItemView.findViewById(R.id.text_view_time);
-        String formattedTime = formatTime(dateObject);
-        timeTextView.setText(formattedTime);
-
         return listItemView;
     }
 
