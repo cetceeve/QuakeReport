0b2e4825225b3b3d7b58d848a05a7eec0df12098
Fabian Zeiher <fzeiher@gmail.com>
2016-11-02 16:25:37 +0100

    fix: Quake Report App finished

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -129,7 +129,7 @@ public class EarthquakeActivity extends AppCompatActivity implements android.app
         Uri.Builder uriBuilder = baseUri.buildUpon();
 
         uriBuilder.appendQueryParameter("format", "geojson");
-        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("limit", "100");
         uriBuilder.appendQueryParameter("minmag", minMagnitude);
         uriBuilder.appendQueryParameter("orderby", orderBy);
 
