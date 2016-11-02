76566ecc7c9268a18911e77b52e47cd73980fc4d
Fabian Zeiher <fzeiher@gmail.com>
2016-11-02 16:03:19 +0100

    feat: add orderby settings (settings finished)

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -118,6 +118,12 @@ public class EarthquakeActivity extends AppCompatActivity implements android.app
         String minMagnitude = sharedPrefs.getString(
                 getString(R.string.settings_min_magnitude_key),
                 getString(R.string.settings_min_magnitude_default));
+
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
         Uri baseUri = Uri.parse(USGS_REQUEST_URL);
         Uri.Builder uriBuilder = baseUri.buildUpon();
 
